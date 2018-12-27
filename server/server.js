1a8c344d2d47a346dae5efa24ea81e738662c46d
raavr <rafal.r26@gmail.com>
2018-12-27 20:19:00 +0100

    add a simple server (with mock data and a login route)

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+const SECRET_PASS = 'secret1234pass!';
+const USERS = {
+  'admin@example.com': {
+    id: 1,
+    role: 'admin',
+    passwordHash: '92295db3afeb4aeb39abe41c7d596a7833f1362d6b6013d43dfa89b96d3449f3' //admin123
+  },
+  'user@example.com': {
+    id: 2,
+    role: 'user',
+    passwordHash: '5081e47a658989f140abc1e83f7640c13c26a0b82ecb89b516d9c9c297d97adc' //user123
+  }
+}
+
+function isCorrectPassword(email, password) {
+  const passwordHash = USERS[email].passwordHash;
+  const reqHash = crypto.createHmac('sha256', SECRET_PASS).update(password).digest('hex');
+  return reqHash === passwordHash;
+}
+
+function genToken(data) {
+  return jwt.sign(data, SECRET_PASS, { expiresIn: '1h' });
+}
+
+app.post('/login', (req, res) => {
+  const user = USERS[req.body.email];
+  if(!!user) {
+    if(isCorrectPassword(req.body.email, req.body.password)) {
+      const token = genToken({ id: user.id, role: user.role });
+      res.send({ token });
+    } else {
+      res.status(401).send({message: 'Invalid email or password'});
+    }
+  } else {
+    res.status(401).send({message: 'Invalid email or password'});
+  }
+})
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
