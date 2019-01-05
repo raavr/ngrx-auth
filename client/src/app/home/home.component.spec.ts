176e5bad781ce02f0cfc17e4affaa77c6a80274c
raavr <rafal.r26@gmail.com>
2019-01-05 14:27:42 +0100

    fix the home.component test

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { StoreModule, combineReducers, Store } from '@ngrx/store';
+import * as fromAuth from '../auth/reducers';
+import { DecodeTokenSuccess } from '../auth/actions/auth.actions';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let store: Store<fromAuth.State>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [ 
+        StoreModule.forRoot({
+          auth: combineReducers(fromAuth.reducers),
+        }),
+       ],
+      declarations: [ HomeComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
+    store = TestBed.get(Store);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
-
+  
   it('should create', () => {
+    store.dispatch(new DecodeTokenSuccess({ name: 'Test', id: '1'}));
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
 });
