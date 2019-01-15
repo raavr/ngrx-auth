d3cbea52fbe7de44fa5ce140018273e0429a822e
raavr <rafal.r26@gmail.com>
2019-01-15 13:23:06 +0100

    fix e2e tests

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -7,8 +7,10 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display toolbar name', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngRxCounter!');
+    page.getToolbarText().then((text) => {
+      expect(text).toContain('NgRx Auth App');
+    })
   });
 });
